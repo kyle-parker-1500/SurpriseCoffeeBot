e4ce84f4a40c11834844baf1dbf9b8ce7fa2531f
kyle-parker-1500 <kylerparker1500@gmail.com>
2026-01-16 23:28:08 -0800

    added build command

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Surprise Coffee Generator Bot
 Use `;surprise` to generate a coffee.
+
 You can use `;help surprise` to generate a `.md` file detailing how the command works.
+
 The bot must be loaded into your server.
--- coffee-bot-code/main.py
+++ coffee-bot-code/main.py
@@ -22,45 +22,125 @@ db = {
     'syrup': ['Vanilla', 'Caramel', 'Hazelnut', 'Chocolate', 'Lavender', 'Banana'],
     'toppings': ['Whipped Cream', 'Nutmeg', 'Cinnamon', 'Honey'],
 }
-# bot commands
-@bot.command(name='surprise')
-async def surprise_coffee(ctx, beans: Optional[str] = None, syrup: Optional[str] = None, toppings: Optional[str] = None):
+
+
+# generating output string for v1 of the coffee bot 1/16/26
+def generate_output():
     beans_list = []
     syrup_list = []
     toppings_list = []
 
-
-    num_syrup = random.randint(1, len(db['syrup'])//2)
-    num_toppings = random.randint(1, len(db['toppings'])//2)
+    num_syrup = random.randint(1, len(db['syrup']))
+    num_toppings = random.randint(1, len(db['toppings']))
     
     # get random bean type
     beans_list.append(random.choice(db['beans']))
     
+    # get random syrups if they don't already exist in the coffee
     for i in range(0, num_syrup):
-        syrup_list.append(random.choice(db['syrup']))
+        temp_syrup = random.choice(db['syrup'])
+        if temp_syrup not in syrup_list:
+            syrup_list.append(temp_syrup)
         
+    # get random toppings if they don't already exist in the coffee
     for i in range(0, num_toppings):
-        toppings_list.append(random.choice(db['toppings']))
+        temp_toppings = random.choice(db['toppings'])
+        if temp_toppings not in toppings_list:
+            toppings_list.append(temp_toppings)
 
+    # generate output string
+    output = ""
+    
+    # add beans to output
+    output += "**Beans:**" + "\n"
+    output += "- " + beans_list[0] + "\n\n"
+    
+    # add syrups to output
+    if len(syrup_list) > 1:
+        output += "**Syrups:**" + "\n"
+    else:
+        output += "**Syrup:**" + "\n"
+
+    for i in range(0, len(syrup_list)):
+        output += "- " + syrup_list[i] + "\n"
+    output += "\n"
+    
+    # add toppings to output
+    if len(toppings_list) > 1:
+        output += "**Toppings:**" + "\n"
+    else:
+        output += "**Topping:**" + "\n"
+        
+    for i in range(0, len(toppings_list)):
+        output += "- " + toppings_list[i] + "\n"
+    output += "\n"
+   
+    return output
+
+# bot commands
+@bot.command(name='surprise')
+async def surprise_coffee(ctx):
+    #
+    # generate output string
+    output = generate_output()
+
+    # send generated coffee
     await ctx.send("======= Generating Surprise Coffee =======")
     async with ctx.typing():
-        # building output string
-        output = ""
-        # beans
-        await ctx.send(f"**Beans:** {beans_list[0]}")
-        
-        # syrups
-        await ctx.send("**Syrup(s):** ")
-        for item in range(0, len(syrup_list)):
-            await ctx.send(f"{syrup_list[item]}")
-            
-        # toppings
-        await ctx.send("**Topping(s):** ")
-        for item in range(0, len(toppings_list)):
-            await ctx.send(f"{toppings_list[item]}")
-        
+        await ctx.send(output)
         await ctx.send("======================================")
         
+
+def process_build(coffee_type, syrup, toppings):
+    coffee_type_list = [item.strip() for item in coffee_type.split(',')]
+    syrup_list = [item.strip() for item in syrup.strip().split(',')]
+    toppings_list = [item.strip() for item in toppings.strip().split(',')]
+
+    # combine & read all lists and create recipe
+    final = ""
+    
+    final += "======= Generating Prebuilt Coffee =======\n"
+    
+    # beans to string
+    final += "**Coffee Type:**\n"
+    for item in coffee_type_list:
+        final += "- " + item
+        final += '\n' 
+    final += '\n'
+        
+    # syrup to string
+    final += "**Syrup:**\n"
+    for item in syrup_list:
+        final += "- " + item
+        final += '\n'
+    final += '\n'
+        
+    # toppings to string
+    final += "**Toppings:**\n"
+    for item in toppings_list:
+        final += "- " + item
+        final += '\n'
+    final += '\n'
+
+    final += "======================================="    
+    
+    return final
+
+
+@bot.command(name='build')
+async def build_coffee(ctx, coffee_type: Optional[str] = "", syrup: Optional[str] = "", toppings: Optional[str] = ""):
+    if coffee_type == "":
+        await ctx.send(f"No coffee type specified.")
+        return
+            
+    final = process_build(coffee_type, syrup, toppings)
+    
+    await ctx.send(final)
+    
+
+
+
+
 
 # get & read discord token
 with open("./TOKEN.secret") as file:
