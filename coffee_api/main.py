f76cbb4040b2a50f34f5fb379dd15379b348aa8f
Kyle Parker <kylerparker1500@gmail.com>
2026-01-20 11:30:53 -0800

    started copying from my other api lol

--- coffee_api/main.py
+++ coffee_api/main.py
@@ -1,11 +1,49 @@
 from fastapi import FastAPI
 from typing import Optional
+from typing import Union 
 from pydantic import BaseModel
+import sqlite3
 
-class Item(BaseModel):
-    id: int
-    name: str
+app = FastAPI()
+
+# pydantic model -> request validation
+class Recipe(BaseModel):
+    title: str
+    ingredients: str
+    instructions: str 
+     
+# setup database
+def get_db():
+    # find .db file inside computer file
+    connector = sqlite3.connect('recipes.db')
     
-@app.get('/')
-async def home():
-    await print("Hello World")
+    connector.row_factory = sqlite3.Row
+
+    return connector
+
+# create table on startup
+def init_db():
+    # get db file
+    connector = get_db()
+
+    # define sql interaction object
+    cursor = connector.cursor()
+
+    # create table for recipes
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS recipes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            ingredients TEXT,
+            instructions TEXT 
+        )
+        '''
+    )
+    
+    connector.commit()
+    connector.close()
+
+# initialize db
+init_db()
+
